47b7661beee764ec8c3b23542c75ebadc9bd4e1f
Durgesh yadav <durgeshyadavgpm16@gmail.com>
2023-09-12 14:46:25 +0530

    Modification & new methods Add

--- src/main/java/com/durgesh/controller/EmployeeRestController.java
+++ src/main/java/com/durgesh/controller/EmployeeRestController.java
@@ -11,44 +11,50 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.OptionalDouble;
 
 @RestController
 @RequestMapping(value = "/emp")
 public class EmployeeRestController {
+    //JAVA-8 <-*-> Concept
     @Autowired
     private EmployeeRepo employeeRepo;
+
     @PostMapping()
-    public Employee save(@RequestBody Employee employee)
-    {
+    public Employee save(@RequestBody Employee employee) {
         return employeeRepo.save(employee);
     }
+
     //get All Employee
     @GetMapping
-    public List<Employee> employees()
-    {
+    public List<Employee> employees() {
         return employeeRepo.findAll();
     }
 
     //get Employee by max-age
     @GetMapping(value = "max-age")
-    public Optional<Employee> getEmployeeInOldestAge()
-    {
-        return employeeRepo.findAll().stream().max((employee, t1) ->Integer.compare(employee.getAge(),t1.getAge()) );
+    public Optional<Employee> getEmployeeInOldestAge() {
+        return employeeRepo.findAll().stream().max((employee, t1) -> Integer.compare(employee.getAge(), t1.getAge()));
     }
+
     @GetMapping(value = "min-age")
-    public Optional<Employee> getEmployeeInYoungestAge()
-    {
-        return employeeRepo.findAll().stream().min((employee, t1) ->Integer.compare(employee.getAge(),t1.getAge()) );
+    public Optional<Employee> getEmployeeInYoungestAge() {
+        return employeeRepo.findAll().stream().min((employee, t1) -> Integer.compare(employee.getAge(), t1.getAge()));
     }
+
     @GetMapping(value = "max-salary")
-    public Optional<Employee> highestSalariedEmployee()
-    {
-        return employeeRepo.findAll().stream().max((employee, t1) ->Integer.compare(employee.getSalary().intValue(),t1.getSalary().intValue()) );
+    public Optional<Employee> highestSalariedEmployee() {
+        return employeeRepo.findAll().stream().max((employee, t1) -> Integer.compare(employee.getSalary().intValue(), t1.getSalary().intValue()));
     }
+
     @GetMapping(value = "min-salary")
-    public Optional<Employee> lowestSalariedEmployee()
-    {
-        return employeeRepo.findAll().stream().min((employee, t1) ->Integer.compare(employee.getSalary().intValue(),t1.getSalary().intValue()) );
+    public Optional<Employee> lowestSalariedEmployee() {
+        return employeeRepo.findAll().stream().min((employee, t1) -> Integer.compare(employee.getSalary().intValue(), t1.getSalary().intValue()));
     }
+
+    @GetMapping(value = "total-salary")
+    public OptionalDouble budgetOfSalary() {
+        return employeeRepo.findAll().stream().mapToDouble(Employee::getSalary).average();
+    }
+
 }
